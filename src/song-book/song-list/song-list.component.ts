45204fada57a289a453d4f381b65c791befd30fc
Dawid Musialik <d.musialik.sne@gmail.com>
2023-07-28 21:26:23 +0200

    navigation improvement

--- src/song-book/song-list/song-list.component.ts
+++ src/song-book/song-list/song-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core'
 import { SimpleSongDTO } from '../../songs-api-client'
 import { Observable } from 'rxjs';
 import { SongRepositoryService } from '../song-repository.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'sb-song-list',
@@ -13,14 +14,29 @@ export class SongListComponent implements OnInit {
   searchValue: string = ''
   songs$?: Observable<SimpleSongDTO[]>
 
-  constructor(private repository: SongRepositoryService) { }
+  constructor(
+    private repository: SongRepositoryService,
+    private router: Router) { }
 
   ngOnInit(): void {
     this.songs$ = this.repository.getSimpleSongs()
   }
 
-  onSongsRefresh() {
+  onSongsRefresh(): void {
     this.repository.fetchSimpleSongs()
+  }
+
+  getProperUrl(songId?: string): string {
+    if (songId === undefined) {
+      songId = ''
+    }
+
+    if (this.router.url.endsWith('/edit')) {
+      return songId + '/edit'
+    }
+    else {
+      return songId
+    }
   }
 }
 
